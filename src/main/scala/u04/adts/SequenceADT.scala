fa0e79eeb3b5adc84ae9933417b5ea17710bca46
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2024-07-29 17:21:10 +0200

    add filter as operation in sequence ADT

--- src/main/scala/u04/adts/SequenceADT.scala
+++ src/main/scala/u04/adts/SequenceADT.scala
@@ -19,13 +19,18 @@ object SequenceADT:
     def map[B](mapper: A => B): Sequence[B] = seq match
       case Cons(h, t) => Cons(mapper(h), t.map(mapper))
       case Nil()      => Nil()
-  
+
+    def filter(f: A => Boolean): Sequence[A] = seq match
+      case Cons(h, t) if f(h) => Cons(h, t.filter(f))
+      case Cons(h, t) => t.filter(f)
+      case Nil() => Nil()
+
   // operations as standard methods
   def concat[A](seq1: Sequence[A], seq2: Sequence[A]): Sequence[A] = seq1 match
     case Cons(h, t) => Cons(h, concat(t, seq2))
     case Nil()      => seq2
 
-@main def trySequenceADT =
+@main def trySequenceADT(): Unit =
   import SequenceADT.* 
   val seq = cons(10, cons(20, cons(30, nil())))
   println(concat(seq, seq.map(_ + 1)))
