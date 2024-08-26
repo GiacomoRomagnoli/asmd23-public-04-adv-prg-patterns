d43113fd4e619e07311258a120b1d3c763195a9c
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2024-08-26 16:28:53 +0200

    prove all axioms for monads

--- src/test/scala/u04/monads/MonadAxioms.scala
+++ src/test/scala/u04/monads/MonadAxioms.scala
@@ -6,21 +6,20 @@ import org.scalacheck.Prop.forAll
 import org.scalacheck.{Arbitrary, Cogen, Gen, Properties}
 import u04.monads.Monads.Monad
 import u04.monads.Optionals.{Optional, given}
-import u04.monads.Sequences
 import u04.monads.Sequences.{Sequence, given}
 
-import scala.reflect.ClassTag
-
 object MonadAxioms extends Properties("Monad"):
-  // Monads
-  def monad[T[_]: Monad]: Gen[Monad[T]] = summon[Monad[T]]
+
   // Optionals
+  def optMapper[A, B](using cg: Cogen[A], b: Arbitrary[B]): Gen[A => Optional[B]] = function1(opt[B])
   def opt[A](using a: Arbitrary[A]): Gen[Optional[A]] = oneOf(just[A], empty[A])
   def empty[A]: Gen[Optional[A]] = const(Optional.Empty[A]())
   def just[A](using a: Arbitrary[A]): Gen[Optional[A]] = for
     v <- a.arbitrary
   yield Optional.Just(v)
+
   // Sequences
+  def seqMapper[A, B](using cg: Cogen[A], b: Arbitrary[B]): Gen[A => Sequence[B]] = function1(seq[B])
   def seq[A](using a: Arbitrary[A]): Gen[Sequence[A]] = oneOf(nil[A], cons[A])
   def nil[A]: Gen[Sequence[A]] = const(Sequence.Nil[A]())
   def cons[A](using a: Arbitrary[A]): Gen[Sequence[A]] = for
@@ -28,8 +27,25 @@ object MonadAxioms extends Properties("Monad"):
     s <- seq
   yield Sequence.Cons(v, s)
 
-  //property("flatMap(unit(a), f) == f(a)") =
-    //forAll(oneOf(Sequence, Optional)): t =>
+  // Axioms
+  property("flatMap(unit(a), f) == f(a)") =
+    forAll(arbitrary[Int], optMapper[Int, String]): (a, f) =>
+      summon[Monad[Optional]].unit(a).flatMap(f) == f(a)
+    &&
+    forAll(arbitrary[Int], seqMapper[Int, String]): (a, f) =>
+      summon[Monad[Sequence]].unit(a).flatMap(f) == f(a)
 
+  property("flatMap(m,unit(_)) == m") =
+    forAll(opt[Int], arbitrary[String]): (m, s) =>
+      m.flatMap(_ => summon[Monad[Optional]].unit(s)) == m
+    &&
+    forAll(seq[Int], arbitrary[String]): (m, s) =>
+      m.flatMap(_ => summon[Monad[Sequence]].unit(s)) == m
 
+  property("flatMap(flatMap(m, f), g) == flatMap(m, x => flatMap(f(x), g))") =
+    forAll(opt[Int], optMapper[Int, String], optMapper[String, Int]): (m, f, g) =>
+      m.flatMap(f).flatMap(g) == m.flatMap(x => f(x).flatMap(g))
+    &&
+    forAll(seq[Int], seqMapper[Int, String], seqMapper[String, Int]): (m, f, g) =>
+      m.flatMap(f).flatMap(g) == m.flatMap(x => f(x).flatMap(g))
 
